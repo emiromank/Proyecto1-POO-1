1b698cac6d33e458866aa31480ca85119efa76e9
JosePabloMonteroC <pepemonterok2002@gmail.com>
2022-03-08 16:32:29 -0600

    .

--- Project1/src/com/jpmcers/project1/Main.java
+++ Project1/src/com/jpmcers/project1/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		DataWorker obj1 = new DataWorker();
-		obj1.listPalabras = obj1.regresaArrayList("C:\\Users\\Roman\\Downloads\\f1Tweets\\F1Resumido.csv");
+		obj1.listPalabras = obj1.regresaArrayList("C:\\Users\\pepem\\git\\Proyecto1-POO\\Project1\\temporal1.csv");
 		obj1.listPalabras = obj1.eliminarCaracteres(obj1.listPalabras);
 		
 		
